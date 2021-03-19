3662999e7706440d090bd1744ea80f28a12fe456
Justin Griffin <griffin.justintime@gmail.com>
2021-03-19 10:06:40 -0400

    added functions and arrays

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+var lowercaseArray = [];
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -8,17 +8,41 @@ function writePassword() {
 
   passwordText.value = password;
 }
+function userOptions() {
+  var passlength = parseInt(window.prompt("Please choose a password length."));
+  // console.log(typeof passlength);
+  if (passlength < 8) {
+    window.alert("Password must be at least 8 characters.");
+  }
 
+  var passlower = window.prompt("Include lowercase characters?");
+  var passupper = window.prompt("Include uppercase characters?");
+  var passnumeric = window.prompt("Include numeric characters?");
+  var passspec = window.prompt("Include special characters?");
+  var options = {
+    passlower: passlower,
+    passlength: passlength,
+  };
+  console.log(options.passlength);
+
+  return options;
+}
+function randomValue(arr) {
+  var randomIndex = Math.floor(Math.random() * arr.length);
+  var value = arr[randomIndex]
+  return value;
+}
 function generatePassword() {
+  
   // TODO: Write the code that asks the user about the parameters for their password
-
-  var passlength = prompt("Please choose a password length.");
-  var passlower = prompt("Include lowercase characters?");
-  var passupper = prompt("Include uppercase characters?");
-  var passnumeric = prompt("Include numeric characters?");
-  var passspec = prompt("Include special characters?");
-
+  var generatedPassword = []
+  var userOptions = userOptions();
+if (userOptions.passlower) {
+  // I need to use array method concat
+  generatedPassword.push(randomValue(lowercaseArray))
+}
   // TODO: use those parameters to generate a new password
+
   // TODO: return that password
 }
 
